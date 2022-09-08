f2e8d3118ff87284b02ddb6feef3ad7fb17aec65
ZieglD <daniziegler322@gmail.com>
2022-09-08 15:40:20 +0200

    Added some pages and navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './styles/style.scss';
 
-const user = "susan";
-console.log(`hello ${user}, welcome to your npm project!`);
-
-for (let i = 0; i < 5; i++) {
-  console.log(`hello ${user}, welcome to your npm project!`);
+function toggleVerticalNav() {
+  let topNavBar = document.getElementById("topNavBar");
+  if (topNavBar.className === "navBar") {
+    topNavBar.className += " responsive";
+  } else {
+    topNavBar.className = "navBar";
+  }
 }
+
+window.toggleVerticalNav = toggleVerticalNav;
