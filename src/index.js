0f228248fc379f48cef0b00f7ede0c2acb270b1e
ZieglD <daniziegler322@gmail.com>
2022-09-08 11:55:49 +0200

    Started with Setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const user = "susan";
+console.log(`hello ${user}, welcome to your npm project!`);
+
+for(let i = 0; i < 5; i++){
+  console.log(`hello ${user}, welcome to your npm project!`);
+}
